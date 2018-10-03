ea11a92e8198a8c6192b96867b1c969b32a4ffe5
lwhamilton03 <lucy.hamilton@academytrainee.com>
2018-10-03 16:45:59 +0100

    initialise

---
+++ src/main/java/com/qa/AddingDatabaseSpringBoot/AddingDatabaseSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.qa.AddingDatabaseSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddingDatabaseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddingDatabaseSpringBootApplication.class, args);
+	}
+}
